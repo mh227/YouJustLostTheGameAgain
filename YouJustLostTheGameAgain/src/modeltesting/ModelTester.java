8eb0e1bdb9b91e337e80f785654f73b552a3abdb
JosephLeffler <joseph.leffler333@gmail.com>
2017-08-18 15:03:06 -0400

    Testing changing local repository affects github.

--- YouJustLostTheGameAgain/src/modeltesting/ModelTester.java
+++ YouJustLostTheGameAgain/src/modeltesting/ModelTester.java
@@ -5,6 +5,7 @@ public class ModelTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.print("test one for github");
 	}
 
 }
