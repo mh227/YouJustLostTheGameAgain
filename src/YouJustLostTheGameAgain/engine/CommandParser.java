300cc5ece3652ada3ba48f186977095dd5696a9d
Mike Henderson <bigmh227@gmail.com>
2017-11-22 15:24:41 -0500

    Switched from Strings to Patterns to make things easier later.

--- src/YouJustLostTheGameAgain/engine/CommandParser.java
+++ src/YouJustLostTheGameAgain/engine/CommandParser.java
@@ -1,20 +1,22 @@
 package YouJustLostTheGameAgain.engine;
 
+import java.util.regex.Pattern;
+
 public class CommandParser {
 	
-	public static final String LOOK_REGEX = "(?i)look( around)?|survey|search";
-	public static final String LOOK_DIRECTION_REGEX = "(?i)(?:(look |view ))(?<direction>n(orth)?|s(outh)?|e(ast)?|w(est)?|up|down|left|right)";
-	public static final String INSPECT_REGEX = "(?i)(?:(inspect |examine |look at |view ))(?<target>.+)";
-	public static final String HELP_REGEX = "(?i)(?:help ?)(?<command>.*)";
-	public static final String RESTART_REGEX = "(?i)restart|reboot|reload|die|kill (self|me)|(?:commit )?suicide";
-	public static final String MOVE_REGEX = "(?i)(?:walk |move |run |go |head )(?<direction>n(orth)?|s(outh)?|e(ast)?|w(est)?|up|down|left|right)";
-	public static final String USE_REGEX = "(?i)(?:use |equip |wear |drink |eat |activate |unlock |unblock )(?<target>.+)";
-	public static final String USE_ON_REGEX = "(?i)(?:use )(?<item>.+)(?: on | with )(?<target>.+)";
-	public static final String TALK_REGEX = "(?i)(?:talk |speak |converse |listen )(?:with |to )(?<target>.+)";
-	public static final String TAKE_REGEX = "(?i)(?:take |pick up |acquire |grab |collect )(?<target>.+)";
-	public static final String DROP_REGEX = "(?i)(?:discard |toss |trash |drop )(?<target>.+)";
-	public static final String ATTACK_REGEX = "(?i)(?:attack |kill |murder |maim |hurt |punch |injure )(?<target>.+)";
-	public static final String SAVE_REGEX = "(?i)sa+ve( me+)?";
+	public static final Pattern LOOK_REGEX = Pattern.compile("(?i)look( around)?|survey|search");
+	public static final Pattern LOOK_DIRECTION_REGEX = Pattern.compile("(?i)(?:(look |view ))(?<direction>n(orth)?|s(outh)?|e(ast)?|w(est)?|up|down|left|right)");
+	public static final Pattern INSPECT_REGEX = Pattern.compile("(?i)(?:(inspect |examine |look at |view ))(?<target>.+)");
+	public static final Pattern HELP_REGEX = Pattern.compile("(?i)(?:help ?)(?<command>.*)");
+	public static final Pattern RESTART_REGEX = Pattern.compile("(?i)restart|reboot|reload|die|kill (self|me)|(?:commit )?suicide");
+	public static final Pattern MOVE_REGEX = Pattern.compile("(?i)(?:walk |move |run |go |head )(?<direction>n(orth)?|s(outh)?|e(ast)?|w(est)?|up|down|left|right)");
+	public static final Pattern USE_REGEX = Pattern.compile("(?i)(?:use |equip |wear |drink |eat |activate |unlock |unblock )(?<target>.+)");
+	public static final Pattern USE_ON_REGEX = Pattern.compile("(?i)(?:use )(?<item>.+)(?: on | with )(?<target>.+)");
+	public static final Pattern TALK_REGEX = Pattern.compile("(?i)(?:talk |speak |converse |listen )(?:with |to )(?<target>.+)");
+	public static final Pattern TAKE_REGEX = Pattern.compile("(?i)(?:take |pick up |acquire |grab |collect )(?<target>.+)");
+	public static final Pattern DROP_REGEX = Pattern.compile("(?i)(?:discard |toss |trash |drop )(?<target>.+)");
+	public static final Pattern ATTACK_REGEX = Pattern.compile("(?i)(?:attack |kill |murder |maim |hurt |punch |injure )(?<target>.+)");
+	public static final Pattern SAVE_REGEX = Pattern.compile("(?i)sa+ve( me+)?");
 	
 	//TODO: Hook into engine when possible.
 	public static void parseInput() {
--- test/YouJustLostTheGameAgain/test/CommandParserTest.java
+++ test/YouJustLostTheGameAgain/test/CommandParserTest.java
@@ -13,18 +13,16 @@ public class CommandParserTest {
 	
 	@Test
 	public void testLookRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.LOOK_REGEX);
 		for(String command : Arrays.asList("look", "look around", "survey", "search")) {
-			Assertions.assertTrue(pattern.matcher(command).matches(), String.format("No match on %s.", command));
+			Assertions.assertTrue(CommandParser.LOOK_REGEX.matcher(command).matches(), String.format("Expected match for %s.", command));
 		}
 	}
 	
 	@Test
 	public void testLookDirectionRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.LOOK_DIRECTION_REGEX);
 		for(String command : Arrays.asList("look", "view")) {
 			for(String direction : Arrays.asList("n", "s", "e", "w", "north", "south", "east", "west", "up", "down", "left", "right")) {
-				Matcher matcher = pattern.matcher(command + " " + direction);
+				Matcher matcher = CommandParser.LOOK_DIRECTION_REGEX.matcher(command + " " + direction);
 				Assertions.assertTrue(matcher.matches() && direction.equals(matcher.group("direction")), String.format("Expected %s %s to return %s, got %s", command, direction, direction, matcher.group("direction")));
 			}
 		}
@@ -32,35 +30,31 @@ public class CommandParserTest {
 	
 	@Test
 	public void testInspectRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.INSPECT_REGEX);
 		for(String command : Arrays.asList("inspect", "examine", "look at", "view")) {
-			Matcher matcher = pattern.matcher(command + " sword");
+			Matcher matcher = CommandParser.INSPECT_REGEX.matcher(command + " sword");
 			Assertions.assertTrue(matcher.matches() && "sword".equals(matcher.group("target")), String.format("Expected %s sword to return sword, got %s.", command, matcher.group("target")));
 		}
 	}
 	
 	@Test
 	public void testHelpRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.HELP_REGEX);
-		Matcher matcher = pattern.matcher("help inspect");
+		Matcher matcher = CommandParser.HELP_REGEX.matcher("help inspect");
 		Assertions.assertTrue(matcher.matches() && "inspect".equals(matcher.group("command")), String.format("Expected help inspect to return inspect, got %s", matcher.group("command")));
-		Assertions.assertTrue(pattern.matcher("help").matches());
+		Assertions.assertTrue(CommandParser.HELP_REGEX.matcher("help").matches());
 	}
 	
 	@Test
 	public void testRestartRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.RESTART_REGEX);
 		for(String command : Arrays.asList("restart", "reboot", "reload", "die", "kill self", "kill me", "suicide", "commit suicide")) {
-			Assertions.assertTrue(pattern.matcher(command).matches(), String.format("Expected match for %s.", command));
+			Assertions.assertTrue(CommandParser.RESTART_REGEX.matcher(command).matches(), String.format("Expected match for %s.", command));
 		}
 	}
 	
 	@Test
 	public void testMoveRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.MOVE_REGEX);
 		for(String command : Arrays.asList("walk", "run", "go", "move", "head")) {
 			for(String direction : Arrays.asList("n", "s", "e", "w", "north", "south", "east", "west", "up", "down", "left", "right")) {
-				Matcher matcher = pattern.matcher(command + " " + direction);
+				Matcher matcher = CommandParser.MOVE_REGEX.matcher(command + " " + direction);
 				Assertions.assertTrue(matcher.matches() && direction.equals(matcher.group("direction")), String.format("Expected %s %s to return %s, got %s", command, direction, direction, matcher.group("direction")));
 			}
 		}
@@ -68,18 +62,16 @@ public class CommandParserTest {
 	
 	@Test
 	public void testUseRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.USE_REGEX);
 		for(String command : Arrays.asList("use", "equip", "wear", "drink", "eat", "activate", "unlock", "unblock")) {
-			Matcher matcher = pattern.matcher(command + " thing");
+			Matcher matcher = CommandParser.USE_REGEX.matcher(command + " thing");
 			Assertions.assertTrue(matcher.matches() && "thing".equals(matcher.group("target")), String.format("Expected %s thing to return thing, got %s.", command, matcher.group("target")));
 		}
 	}
 	
 	@Test
 	public void testUseOnRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.USE_ON_REGEX);
 		for(String command : Arrays.asList("use thing1 on thing2", "use thing1 with thing2")) {
-			Matcher matcher = pattern.matcher(command);
+			Matcher matcher = CommandParser.USE_ON_REGEX.matcher(command);
 			Assertions.assertTrue(matcher.matches() && "thing1".equals(matcher.group("item")), String.format("Expected %s to return thing1, got %s.", command, matcher.group("item")));
 			Assertions.assertTrue("thing2".equals(matcher.group("target")), String.format("Expected %s to return thing2, got %s.", command, matcher.group("target")));
 		}
@@ -87,10 +79,9 @@ public class CommandParserTest {
 	
 	@Test
 	public void testTalkRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.TALK_REGEX);
 		for(String command1 : Arrays.asList("talk", "speak", "converse", "listen")) {
 			for(String command2 : Arrays.asList("with", "to")) {
-				Matcher matcher = pattern.matcher(command1 + " " + command2 + " thing");
+				Matcher matcher = CommandParser.TALK_REGEX.matcher(command1 + " " + command2 + " thing");
 				Assertions.assertTrue(matcher.matches() && "thing".equals(matcher.group("target")), String.format("Expected %s %s thing to return thing, got %s.", command1, command2, matcher.group("target")));
 			}
 		}
@@ -98,36 +89,32 @@ public class CommandParserTest {
 	
 	@Test
 	public void testTakeRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.TAKE_REGEX);
 		for(String command : Arrays.asList("take", "pick up", "acquire", "grab", "collect")) {
-			Matcher matcher = pattern.matcher(command + " thing");
+			Matcher matcher = CommandParser.TAKE_REGEX.matcher(command + " thing");
 			Assertions.assertTrue(matcher.matches() && "thing".equals(matcher.group("target")), String.format("Expected %s thing to return thing, got %s.", command, matcher.group("target")));
 		}
 	}
 	
 	@Test
 	public void testDropRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.DROP_REGEX);
 		for(String command : Arrays.asList("discard", "toss", "trash", "drop")) {
-			Matcher matcher = pattern.matcher(command + " thing");
+			Matcher matcher = CommandParser.DROP_REGEX.matcher(command + " thing");
 			Assertions.assertTrue(matcher.matches() && "thing".equals(matcher.group("target")), String.format("Expected %s thing to return thing, got %s.", command, matcher.group("target")));
 		}
 	}
 	
 	@Test
 	public void testAttackRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.ATTACK_REGEX);
 		for(String command : Arrays.asList("attack", "kill", "murder", "maim", "hurt", "punch", "injure")) {
-			Matcher matcher = pattern.matcher(command + " enemy");
+			Matcher matcher = CommandParser.ATTACK_REGEX.matcher(command + " enemy");
 			Assertions.assertTrue(matcher.matches() && "enemy".equals(matcher.group("target")), String.format("Expected %s enemy to return enemy, got %s.", command, matcher.group("target")));
 		}
 	}
 	
 	@Test
 	public void testSaveRegex() {
-		Pattern pattern = Pattern.compile(CommandParser.SAVE_REGEX);
 		for(String command : Arrays.asList("save", "save me", "saaave meee")) {
-			Assertions.assertTrue(pattern.matcher(command).matches(), String.format("Expected match for %s.", command));
+			Assertions.assertTrue(CommandParser.SAVE_REGEX.matcher(command).matches(), String.format("Expected match for %s.", command));
 		}		
 	}
 
